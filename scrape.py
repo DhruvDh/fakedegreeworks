9b687b85c9af62ed4f88baa69f503ea7500a90ba
Dhruv <dhruv.dhamani@gmail.com>
2018-06-29 19:20:57 +0530

    init

--- scrape.py
+++ scrape.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import os
+
+Catalog = {}
 
 driver = webdriver.Chrome()
 driver.get("https://catalog.uncc.edu/preview_program.php?catoid=23&poid=5578")
-assert "Program" in driver.title
+soup = BeautifulSoup(driver.page_source, 'lxml')
+Catalog["name"] = str(soup.find(id="acalog-page-title").string).strip()
+
+print(Catalog["name"])
+
 driver.close()
